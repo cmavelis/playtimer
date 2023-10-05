0f6113629b1921ab73c124bf4565163a036fb590
cmavelis <cmavelis@gmail.com>
2023-10-05 09:48:17 -0700

    tutorial follow along

--- tutorial/MainPage.xaml.cs
+++ tutorial/MainPage.xaml.cs
@@ -1,25 +1,14 @@
-﻿namespace tutorial;
+﻿using tutorial.ViewModel;
+namespace tutorial;
+
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public MainPage(MainViewModel vm)
+    {
+        InitializeComponent();
+        BindingContext = vm;
+    }
 }
 
 
