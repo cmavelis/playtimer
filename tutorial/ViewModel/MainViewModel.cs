b1aa0cbc2c5d47e570b9b7be7b3f975c413bf3ac
cmavelis <cmavelis@gmail.com>
2023-10-09 21:39:50 -0700

    add timer

--- tutorial/ViewModel/MainViewModel.cs
+++ tutorial/ViewModel/MainViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Threading;
+using System.Timers;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 
@@ -9,14 +12,48 @@ public partial class MainViewModel : ObservableObject
 	public MainViewModel ()
 	{
 		Items = new ObservableCollection<string>();
-	}
-	[ObservableProperty]
+        SetTimer();
+        Start();
+    }
+
+    readonly Stopwatch stopwatch = new();
+    private static System.Timers.Timer aTimer;
+
+    [ObservableProperty]
 	ObservableCollection<string> items;
 
 	[ObservableProperty]
 	string text;
 
-	[RelayCommand]
+	[ObservableProperty]
+	string elapsed;
+
+    [ObservableProperty]
+    bool isRunning;
+
+    private void SetTimer()
+    {
+        aTimer = new System.Timers.Timer(1000);
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+    }
+
+    private void OnTimedEvent(Object source, ElapsedEventArgs e)
+    {
+        Elapsed = stopwatch.Elapsed.ToString(@"mm\:ss");
+    }
+
+    private void Start()
+    {
+        stopwatch.Restart();
+
+        IsRunning = true;
+
+        aTimer.Enabled = true;
+    }
+
+    [RelayCommand]
 	void Add()
 	{
 		// add item
